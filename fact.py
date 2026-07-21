90ed37337e001b4b93c22a00708fcfdf6fdce3a3
chinmaiawate <chinmai.awate25@mmit.edu.in>
2026-07-21 12:08:12 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number:"))
+def factorial(n):
+    
+    if n == 0 or n == 1:
+        return 1
+    else:
+     return n * factorial(n - 1)
+
+print(f"The factorial of {n} is {factorial(n)}")
+
